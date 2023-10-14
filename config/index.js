0f5880e08a7ea89c06892ab510b73861afd2c6ac
Praveen <vrcwebsolution@gmail.com>
2023-10-14 09:09:22 +0530

    url change

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev 
     ? "https://whollykart.com/admin" 
-    : "https://nest-nextjs.vercel.app";
+    : "https://whollykart.com/admin";
